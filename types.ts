422bacb74e376b447cbc96ca86335825222132f7
FarizioKautsar <kheruzy@gmail.com>
2025-08-28 23:00:53 +1000

    Mock the ingredients

---
+++ types.ts
@@ -0,0 +1,26 @@
+export enum IngredientType {
+  Vegetable = "vegetable",
+  Grain = "grain",
+  Dairy = "dairy",
+  Meat = "meat",
+  Protein = "protein",
+  Fruit = "fruit",
+  Spice = "spice",
+  Seafood = "seafood",
+  Nut = "nut",
+  Other = "other",
+}
+
+export type Ingredient = {
+  id: string;
+  emoji: string;
+  name: string;
+  amount: string;
+  units: string[];
+  nutritionsPer100g?: {
+    protein: number;
+    carbs: number;
+    fat: number;
+  };
+  types: IngredientType[];
+};
